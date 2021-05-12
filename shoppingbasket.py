0832351ab5b3e8dbd3a59299b0746de1ad232937
jas <jasmine-lim88@hotmail.com>
2021-05-12 14:37:48 +0800

    added comments

--- shoppingbasket.py
+++ shoppingbasket.py
@@ -8,4 +8,9 @@ class Basket(object):
     items: List[Item]
 
     def total(self):
+        # how reduce works:
+        # At first step, first two elements of sequence are picked and the result is obtained.
+        # Next step is to apply the same function to the previously attained result and the number just succeeding the second element and the result is again stored.
+        # This process continues till no more elements are left in the container
+        # from: https://www.geeksforgeeks.org/reduce-in-python/
         return reduce(lambda subtotal, item: subtotal + (item.unit_price * item.quantity), self.items, 0)
